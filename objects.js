924d7ed966a64688045763d8c2e004c0e5d511a9
Cecelia Smith <ceceliacreates@gmail.com>
2018-12-07 21:00:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,12 @@
 var playlist = { 'Rilo Kiley': 'More Adventurous' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
  playlist[artistName] = songTitle
  return playlist
+=======
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+>>>>>>> b9a7d832bc420963f07b3aaedcbe8dffbe6c099e
 }
 
 function removeFromPlaylist(playlist, artistName) {
